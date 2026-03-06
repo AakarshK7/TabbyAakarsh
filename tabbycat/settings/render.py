23e8a98310f7b9c6b61b7feb39caba66607e1cbc
Sparsh <sparshag2803@gmail.com>
2026-03-06 14:34:14 +0530

    ...

--- tabbycat/settings/render.py
+++ tabbycat/settings/render.py
@@ -93,9 +93,9 @@ if not os.environ.get('DISABLE_SENTRY'):
 INSTALLED_APPS += ('django.contrib.postgres',)
 
 # Email settings
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.sendgrid.net')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'selfcalicotab@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'hohuqxlzctbgwyzh')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'apikey')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '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')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'selfcalicotab@gmail.com')
